58841456aed1799ec752a3607c64b5851c751498
Rafael Franke <frankerafael@bikeflip.com>
2026-02-27 16:28:49 +0100

    chore(observability): integrate sentry error tracking

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -5,6 +5,7 @@ use Illuminate\Foundation\Configuration\Exceptions;
 use Illuminate\Foundation\Configuration\Middleware;
 use App\Http\Middleware\AttachRequestContext;
 use App\Http\Middleware\NoIndexSensitivePaths;
+use Sentry\Laravel\Integration;
 
 return Application::configure(basePath: dirname(__DIR__))
     ->withRouting(
@@ -17,5 +18,5 @@ return Application::configure(basePath: dirname(__DIR__))
         $middleware->append(NoIndexSensitivePaths::class);
     })
     ->withExceptions(function (Exceptions $exceptions): void {
-        //
+        Integration::handles($exceptions);
     })->create();
